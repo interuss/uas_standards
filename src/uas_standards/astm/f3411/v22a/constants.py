ced8f0e5d572928721c12481ca42fffeddb61559
Maximilien Cuony <the-glu@users.noreply.github.com>
2025-02-11 17:43:30 +0100

    [f3411/v22a/constants] Add timestamp resolution (#23)
    * [f3411/v22a/constants] Add timestamp resolution
    
    * Update src/uas_standards/astm/f3411/v22a/constants.py

--- src/uas_standards/astm/f3411/v22a/constants.py
+++ src/uas_standards/astm/f3411/v22a/constants.py
@@ -61,3 +61,6 @@ SpecialTrackDirection = 361
 
 MinTrackDirectionResolution = 1
 """Minimum resolution of track direction value, in degrees, according to definitions in Table 1."""
+
+MinTimestampResolution = 0.1
+"""Minimum resolution of timestamp value, in seconds, according to definitions in Table 1."""
