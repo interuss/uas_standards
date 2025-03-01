f383921d849758d63ed4e261d7d0a0be2978436a
Mickaël Misbach <mickael.misbach@orbitalize.com>
2025-03-01 02:27:14 +0100

    [f3411/v22a/constants] Fix min & max track direction (#22)
    * [f3411/v22a/constants] Fix min & max track direction
    
    * Update src/uas_standards/astm/f3411/v22a/constants.py
    
    Co-authored-by: Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
    
    * Update src/uas_standards/astm/f3411/v22a/constants.py
    
    Co-authored-by: Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>

--- src/uas_standards/astm/f3411/v22a/constants.py
+++ src/uas_standards/astm/f3411/v22a/constants.py
@@ -50,11 +50,11 @@ MinHeightResolution = 1
 SpecialHeight = -1000
 """Special value for height indicating Invalid, No Value or Unknown, according to definitions in Table 1."""
 
-MinTrackDirection = -359
-"""Minimum value for track direction, in degrees, according to definitions in Table 1."""
+MinTrackDirection = 0
+"""Minimum value (inclusive) for track direction, in degrees, according to OpenAPI specification."""
 
-MaxTrackDirection = 359
-"""Maximum value for track direction, in degrees, according to definitions in Table 1."""
+MaxTrackDirection = 360
+"""Maximum value (exclusive) for track direction, in degrees, according to range described in Table 6.  The SpecialTrackDirection value is also allowed."""
 
 SpecialTrackDirection = 361
 """Special value for track direction indicating Invalid, No Value or Unknown, according to definitions in Table 1."""
