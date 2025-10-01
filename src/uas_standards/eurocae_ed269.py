c66858f0c98a28c1001eabff462a2ed8029859de
Michael Barroco <michael@orbitalize.com>
2025-10-01 09:57:15 +0200

    Update src/uas_standards/eurocae_ed269.py
    Co-authored-by: Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>

--- src/uas_standards/eurocae_ed269.py
+++ src/uas_standards/eurocae_ed269.py
@@ -134,7 +134,7 @@ class UASZoneVersion(ImplicitDict):
     name: str | None  # max length: 200
     type: str
     restriction: Restriction
-    restrictionConditions: str | None
+    restrictionConditions: str | list[str] | None
     region: int | None
     reason: list[Reason] | None  # max length: 9
     otherReasonInfo: str | None  # max length: 30
